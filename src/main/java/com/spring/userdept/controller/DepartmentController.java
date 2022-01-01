ef1fce74ec12901b5c50b4fdb649bf862e05fa38
mauroslucios <mauroslucios@gmail.com>
2022-01-01 20:10:12 -0300

    Criados DepartmentController, métodos list, save

--- src/main/java/com/spring/userdept/controller/DepartmentController.java
+++ src/main/java/com/spring/userdept/controller/DepartmentController.java
@@ -1,13 +1,23 @@
 package com.spring.userdept.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.userdept.dto.DepartmentDTO;
+import com.spring.userdept.model.Department;
 import com.spring.userdept.service.DepartmentService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value="/api")
@@ -18,6 +28,16 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 	
-	
+	@GetMapping("/department")
+	@ApiOperation(value="Retorna uma lista de departamentos")
+	public List<DepartmentDTO> listAll(){
+		return departmentService.findAll();
+	}
 
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value="Salva um deparatamento no banco")
+	@PostMapping("/department")
+	public Department insertDepartment(@RequestBody Department department) {
+		return departmentService.insertDepartment(department);
+	}
 }
--- src/main/java/com/spring/userdept/service/DepartmentService.java
+++ src/main/java/com/spring/userdept/service/DepartmentService.java
@@ -1,12 +1,27 @@
 package com.spring.userdept.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.spring.userdept.dto.DepartmentDTO;
+import com.spring.userdept.model.Department;
 import com.spring.userdept.repository.DepartmentRepository;
 
 public class DepartmentService {
 	
 	@Autowired
-	DepartmentRepository depratmentRepository;
+	DepartmentRepository departmentRepository;
+
+	public List<DepartmentDTO> findAll() {
+		List<Department> list = departmentRepository.findAll();
+		return DepartmentDTO.converter(list);
+	}
+
+	public Department insertDepartment(Department department) {
+		return departmentRepository.save(department);
+	}
+	
+	
 
 }
