b319d7f96c6a1103c16b1921ba378f3673b5c978
mauroslucios <mauroslucios@gmail.com>
2023-02-12 13:57:50 -0300

    Atualizando configuração swagger

--- src/main/java/com/spring/userdept/config/SwaggerConfig.java
+++ src/main/java/com/spring/userdept/config/SwaggerConfig.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -21,7 +24,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	  registry.addRedirectViewController("/configuration/ui", "/swagger-resources/configuration/ui");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 
 	@Bean
 	public Docket userDptApi() {
